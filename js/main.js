c8369b12d690e0a438545cbdace59cbda556369f
YassminYousry <yassminyousry4@gmail.com>
2023-09-03 22:55:22 +0300

    final

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@ $(document).ready(function () {
   });
 
   $(".logo").click(500, function () {
-    search.innerHTML="";
+    search.innerHTML = "";
     searchByName("");
   })
-    // ====================== SideBar Links Functions =================
+  // ====================== SideBar Links Functions =================
   // ==========Search=============
   $(".search").click(500, function () {
     closeNav();
@@ -29,16 +29,20 @@ $(document).ready(function () {
   })
   // ==========Category=============
   $(".category").click(500, function () {
+    loadFadeIn();
     closeNav();
     getCategories()
   })
   // ==========Area=============
   $(".area").click(500, function () {
     closeNav();
+
     getArea();
+
   })
   // ==========Ingredient=============
   $(".ingredient").click(500, function () {
+    loadFadeIn();
     closeNav();
     getIngredients();
   })
@@ -50,24 +54,26 @@ $(document).ready(function () {
   // ==================== Global Functions ===================
   function loadFadeIn() {
     $(".spinner").fadeIn(100, function () {
-      $("#loading").css("zIndex", 999)
+      $("#loading").css("zIndex", 999).fadeIn(100)
     })
   }
 
   function loadFadeOut() {
-    $(".spinner").fadeOut(1000)
+    $(".spinner").fadeOut(500, function () {
+      $("#loading").fadeOut(500)
+    })
   }
   // ==================== Image Layer =================
-  function imgHover() {
-    let imgDivHeight = $(".mealImg").height();
+  // function imgHover() {
+  //   let imgDivHeight = $(".mealImg").height();
 
-    $(".mealImg").hover(function () {
-      $(this).children(".layer").animate({ top: 0 }, 500)
+  //   $(".mealImg").hover(function () {
+  //     $(this).children(".layer").animate({ top: 0 }, 500)
 
-    }, function () {
-      $(this).children(".layer").animate({ top: imgDivHeight }, 500)
-    });
-  }
+  //   }, function () {
+  //     $(this).children(".layer").animate({ top: imgDivHeight }, 500)
+  //   });
+  // }
 
   // =================== Close NavBar ==================
   function closeNav() {
@@ -135,7 +141,7 @@ $(document).ready(function () {
     `
     }
     section.innerHTML = divs;
-    imgHover();
+    // imgHover();
     $(".mealImg").click(500, function () {
       let mealId = $(this).attr("mealId");
       mealDetails(mealId);
@@ -225,23 +231,23 @@ $(document).ready(function () {
     </div>
   </div>
   `
-  $("#searchName").keyup(function (e) {
-    let keyValue = e.target.value;
-    if(keyValue === ""){
-      section.innerHTML = ""
-    }else{
-    searchByName(keyValue);
-    }
-  })
+    $("#searchName").keyup(function (e) {
+      let keyValue = e.target.value;
+      if (keyValue === "") {
+        section.innerHTML = ""
+      } else {
+        searchByName(keyValue);
+      }
+    })
 
     section.innerHTML = ""
   }
 
   // ====================== Category =================
   async function getCategories() {
+
     section.innerHTML = ""
     search.innerHTML = "";
-    loadFadeIn();
 
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
     let data = await response.json()
@@ -249,34 +255,34 @@ $(document).ready(function () {
     displayCategories(data.categories)
     loadFadeOut();
 
-}
+  }
 
   // ====================== Display Category =================
   function displayCategories(mealsList) {
     let divs = "";
 
     for (let i = 0; i < mealsList.length; i++) {
-        divs += `
+      divs += `
         <div class="col-md-3">
                 <div strCat = '${mealsList[i].strCategory}' class="mealImg position-relative overflow-hidden rounded-2 cursor-pointer">
                     <img class="w-100" src="${mealsList[i].strCategoryThumb}" alt="" srcset="">
                     <div class="layer position-absolute text-center text-black p-2">
                         <h3>${mealsList[i].strCategory}</h3>
-                        <p class="fw-lighter">${mealsList[i].strCategoryDescription.split(" ").slice(0,20).join(" ")}</p>
+                        <p class="fw-lighter">${mealsList[i].strCategoryDescription.split(" ").slice(0, 20).join(" ")}</p>
                     </div>
                 </div>
         </div>
         `
-      }
-      section.innerHTML = divs;
-      imgHover();
+    }
+    section.innerHTML = divs;
+    // imgHover();
 
     $(".mealImg").click(500, function () {
       strCat = $(this).attr("strCat");
       console.log(strCat);
       getCategoryMeals(strCat)
     })
-}
+  }
   // ====================== Category Meals =================
   async function getCategoryMeals(category) {
     section.innerHTML = ""
@@ -285,30 +291,31 @@ $(document).ready(function () {
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
     let data = await response.json()
 
-    displayMeals(data.meals.slice(0,20))
+    displayMeals(data.meals.slice(0, 20))
     loadFadeOut();
-}
+  }
 
   // ====================== Area =================
   async function getArea() {
+    loadFadeIn();
+
     section.innerHTML = ""
     search.innerHTML = "";
-    loadFadeIn();
 
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
     let data = await response.json()
 
     displayArea(data.meals)
-    loadFadeOut();
+    loadFadeOut()
 
-}
+  }
 
   // ====================== Display Area =================
   function displayArea(mealsList) {
     let divs = "";
 
     for (let i = 0; i < mealsList.length; i++) {
-        divs += `
+      divs += `
         <div class="col-md-3">
                 <div strArea = '${mealsList[i].strArea}' class="rounded-2 text-center cursor-pointer areaDiv text-white m-3">
                         <i class="fa-solid fa-house-laptop fa-4x"></i>
@@ -316,14 +323,14 @@ $(document).ready(function () {
                 </div>
         </div>
         `
-      }
-      section.innerHTML = divs;
+    }
+    section.innerHTML = divs;
 
     $(".areaDiv").click(500, function () {
       strArea = $(this).attr("strArea");
       getAreaMeals(strArea)
     })
-}
+  }
   // ====================== Area Meals =================
   async function getAreaMeals(area) {
     section.innerHTML = ""
@@ -332,46 +339,46 @@ $(document).ready(function () {
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
     let data = await response.json()
 
-    displayMeals(data.meals.slice(0,20))
+    displayMeals(data.meals.slice(0, 20))
     loadFadeOut();
-}
+  }
 
   // ====================== Ingredients =================
   async function getIngredients() {
+
     section.innerHTML = ""
     search.innerHTML = "";
-    loadFadeIn();
 
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`)
     let data = await response.json()
 
     displayIngredients(data.meals.slice(0, 20))
     loadFadeOut();
-}
+  }
 
   // ====================== Display Ingredients =================
   function displayIngredients(mealsList) {
     let divs = "";
 
     for (let i = 0; i < mealsList.length; i++) {
-        divs += `
+      divs += `
         <div class="col-md-3">
                 <div strIngredient='${mealsList[i].strIngredient}' class="rounded-2 text-center cursor-pointer text-white m-3 ingredientsDiv">
                         <i class="fa-solid fa-drumstick-bite fa-4x"></i>
                         <h3>${mealsList[i].strIngredient}</h3>
-                        <p>${(mealsList[i].strDescription).split(" ").slice(0,20).join(" ")}</p>
+                        <p>${(mealsList[i].strDescription).split(" ").slice(0, 20).join(" ")}</p>
                 </div>
         </div>
         `
-      }
-      section.innerHTML = divs;
+    }
+    section.innerHTML = divs;
 
     $(".ingredientsDiv").click(500, function () {
       strIngredient = $(this).attr("strIngredient");
       getIngredientsMeals(strIngredient)
     })
     // onclick="getIngredientsMeals('${arr[i].strIngredient}')"
-}
+  }
   // ====================== Ingredients Meals =================
   async function getIngredientsMeals(area) {
     section.innerHTML = ""
@@ -380,13 +387,13 @@ $(document).ready(function () {
     let response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${area}`)
     let data = await response.json()
 
-    displayMeals(data.meals.slice(0,20))
+    displayMeals(data.meals.slice(0, 20))
     loadFadeOut();
-}
+  }
 
-// ===========================================Contact Us =================================
+  // ===========================================Contact Us =================================
   function showContacts() {
-    search.innerHTML="";
+    search.innerHTML = "";
     section.innerHTML = `<div class="contact min-vh-100 d-flex justify-content-center align-items-center">
     <div class="container w-75 text-center">
         <div class="row g-4">
@@ -434,140 +441,140 @@ $(document).ready(function () {
 
 
     document.getElementById("nameInput").addEventListener("focus", () => {
-        nameInputTouched = true
+      nameInputTouched = true
     })
     $("#nameInput").keyup(function () {
       inputsValidation();
     })
     document.getElementById("emailInput").addEventListener("focus", () => {
-        emailInputTouched = true
+      emailInputTouched = true
     })
     $("#emailInput").keyup(function () {
       inputsValidation();
     })
     document.getElementById("phoneInput").addEventListener("focus", () => {
-        phoneInputTouched = true
+      phoneInputTouched = true
     })
     $("#phoneInput").keyup(function () {
       inputsValidation();
     })
     document.getElementById("ageInput").addEventListener("focus", () => {
-        ageInputTouched = true
+      ageInputTouched = true
     })
     $("#ageInput").keyup(function () {
       inputsValidation();
     })
     document.getElementById("passwordInput").addEventListener("focus", () => {
-        passwordInputTouched = true
+      passwordInputTouched = true
     })
     $("#passwordInput").keyup(function () {
       inputsValidation();
     })
     document.getElementById("repasswordInput").addEventListener("focus", () => {
-        repasswordInputTouched = true
+      repasswordInputTouched = true
     })
     $("#repasswordInput").keyup(function () {
       inputsValidation();
     })
-}
+  }
 
-let nameInputTouched = false;
-let emailInputTouched = false;
-let phoneInputTouched = false;
-let ageInputTouched = false;
-let passwordInputTouched = false;
-let repasswordInputTouched = false;
+  let nameInputTouched = false;
+  let emailInputTouched = false;
+  let phoneInputTouched = false;
+  let ageInputTouched = false;
+  let passwordInputTouched = false;
+  let repasswordInputTouched = false;
 
 
-function inputsValidation() {
+  function inputsValidation() {
     if (nameInputTouched) {
-        if (nameValidation()) {
-            document.getElementById("nameAlert").classList.replace("d-block", "d-none")
+      if (nameValidation()) {
+        document.getElementById("nameAlert").classList.replace("d-block", "d-none")
 
-        } else {
-            document.getElementById("nameAlert").classList.replace("d-none", "d-block")
+      } else {
+        document.getElementById("nameAlert").classList.replace("d-none", "d-block")
 
-        }
+      }
     }
     if (emailInputTouched) {
 
-        if (emailValidation()) {
-            document.getElementById("emailAlert").classList.replace("d-block", "d-none")
-        } else {
-            document.getElementById("emailAlert").classList.replace("d-none", "d-block")
+      if (emailValidation()) {
+        document.getElementById("emailAlert").classList.replace("d-block", "d-none")
+      } else {
+        document.getElementById("emailAlert").classList.replace("d-none", "d-block")
 
-        }
+      }
     }
 
     if (phoneInputTouched) {
-        if (phoneValidation()) {
-            document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
-        } else {
-            document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
+      if (phoneValidation()) {
+        document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
+      } else {
+        document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
 
-        }
+      }
     }
 
     if (ageInputTouched) {
-        if (ageValidation()) {
-            document.getElementById("ageAlert").classList.replace("d-block", "d-none")
-        } else {
-            document.getElementById("ageAlert").classList.replace("d-none", "d-block")
+      if (ageValidation()) {
+        document.getElementById("ageAlert").classList.replace("d-block", "d-none")
+      } else {
+        document.getElementById("ageAlert").classList.replace("d-none", "d-block")
 
-        }
+      }
     }
 
     if (passwordInputTouched) {
-        if (passwordValidation()) {
-            document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
-        } else {
-            document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
+      if (passwordValidation()) {
+        document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
+      } else {
+        document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
 
-        }
+      }
     }
     if (repasswordInputTouched) {
-        if (repasswordValidation()) {
-            document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
-        } else {
-            document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
+      if (repasswordValidation()) {
+        document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
+      } else {
+        document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
 
-        }
+      }
     }
 
 
     if (nameValidation() &&
-        emailValidation() &&
-        phoneValidation() &&
-        ageValidation() &&
-        passwordValidation() &&
-        repasswordValidation()) {
-        submitBtn.removeAttribute("disabled")
+      emailValidation() &&
+      phoneValidation() &&
+      ageValidation() &&
+      passwordValidation() &&
+      repasswordValidation()) {
+      submitBtn.removeAttribute("disabled")
     } else {
-        submitBtn.setAttribute("disabled", true)
+      submitBtn.setAttribute("disabled", true)
     }
-}
+  }
 
-function nameValidation() {
+  function nameValidation() {
     return (/^[a-zA-Z ]+$/.test(document.getElementById("nameInput").value))
-}
+  }
 
-function emailValidation() {
+  function emailValidation() {
     return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("emailInput").value))
-}
+  }
 
-function phoneValidation() {
+  function phoneValidation() {
     return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("phoneInput").value))
-}
+  }
 
-function ageValidation() {
+  function ageValidation() {
     return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("ageInput").value))
-}
+  }
 
-function passwordValidation() {
+  function passwordValidation() {
     return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("passwordInput").value))
-}
+  }
 
-function repasswordValidation() {
+  function repasswordValidation() {
     return document.getElementById("repasswordInput").value == document.getElementById("passwordInput").value
-}
+  }
 });
